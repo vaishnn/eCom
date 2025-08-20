a1dbd63f2922468e08f08619781768a619427fb3
Vaishnav <vaishnavpratap41@gmail.com>
2025-08-20 18:41:23 +0530

    fix

--- websites/croma/cromaSc.py
+++ websites/croma/cromaSc.py
@@ -160,6 +160,7 @@ class CromaServerScraper(CromaScraper):
         options = webdriver.ChromeOptions()
         options.add_argument("--incognito")
         service = Service('/usr/bin/chromedriver') # type: ignore
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
         options.add_experimental_option(
             "prefs", {"profile.default_content_setting_values.geolocation": 2}
         )
